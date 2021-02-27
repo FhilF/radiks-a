74cd752044507b71ea208830a1a0f04da3b277e6
Fhilip Fernandez <fhilipfernandez0209@gmail.com>
2021-02-27 12:41:09 +0800

    Update32

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,7 +10,9 @@ import { SchemaAttribute } from './types';
 
 export const GROUP_MEMBERSHIPS_STORAGE_KEY = 'GROUP_MEMBERSHIPS_STORAGE_KEY';
 
-const cryptoRandomString = require('crypto-random-string');
+const elliptic = require("elliptic");
+
+const ecurve = new elliptic.ec('secp256k1');
 
 const valueToString = (value: any, clazz: any) => {
   if (clazz === Boolean) {
@@ -65,6 +67,36 @@ export const decryptObject = async (encrypted: any, model: Model) => {
   return decrypted;
 };
 
+function aes256CbcEncrypt(iv, key, plaintext) {
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  return Buffer.concat([cipher.update(plaintext), cipher.final()]);
+}
+
+function getHexFromBN(bnInput) {
+  const hexOut = bnInput.toString('hex');
+  if (hexOut.length === 64) {
+      return hexOut;
+  }
+  else if (hexOut.length < 64) {
+      // pad with leading zeros
+      // the padStart function would require node 9
+      const padding = '0'.repeat(64 - hexOut.length);
+      return `${padding}${hexOut}`;
+  }
+  else {
+      throw new Error('Generated a > 32-byte BN for encryption. Failing.');
+  }
+}
+
+function sharedSecretToKeys(sharedSecret) {
+  // generate mac and encryption key from shared secret
+  const hashedSecret = crypto.createHash('sha512').update(sharedSecret).digest();
+  return {
+    encryptionKey: hashedSecret.slice(0, 32),
+    hmacKey: hashedSecret.slice(32)
+  };
+}
+
 export const encryptObject = async (model: Model) => {
   const publicKey = await model.encryptionPublicKey();
   const object = model.attrs;
@@ -88,21 +120,24 @@ export const encryptObject = async (model: Model) => {
 
     const stringValue = valueToString(value, clazz);
 
+    const plainText = stringValue instanceof Buffer ? Buffer.from(stringValue) : Buffer.from(stringValue);
+    const ecPK = ecurve.keyFromPublic(publicKey, 'hex').getPublic();
+    const ephemeralSK = ecurve.genKeyPair();
+    const ephemeralPK = ephemeralSK.getPublic();
+    const sharedSecret = ephemeralSK.derive(ecPK);
+    const sharedSecretHex = getHexFromBN(sharedSecret);
+    const sharedKeys = sharedSecretToKeys(Buffer.from(sharedSecretHex, 'hex'));
+
+
     // const ivdata = crypto.randomBytes(256);
     // const kData = crypto.randomBytes(256);
-    
 
     const keyData = {
       iv: crypto.randomBytes(16),
       key: crypto.randomBytes(32)
     };
 
-    const cipher = crypto.createCipheriv(
-      'aes-256-cbc',
-      keyData.key,
-      keyData.iv,
-    );
-    const cipherText = cipher.update(stringValue, 'utf-8', 'hex') + cipher.final('hex');
+    const cipherText = aes256CbcEncrypt(crypto.randomBytes(16), sharedKeys.encryptionKey, plainText)
     // @ts-ignore
     encrypted[key] = encryptECIES(publicKey, stringValue, true, cipherText);
   });
